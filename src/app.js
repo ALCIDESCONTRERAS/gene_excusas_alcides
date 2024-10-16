9e7d139a3cbebb64d1cb7f8b849652d842851887
ALCIDESCONTRERAS <alcidescon@gmail.com>
2024-10-16 17:25:16 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusa = "Es que mi ";
+  let culpable = ["Perro ", "Gato ", "Loro ", "Gallina ", "Primo "];
+  let hizo = ["me mojó ", "me mordió ", "tiró ", "quemó ", "me pateó "];
+  let evitar = [
+    "la tarea ",
+    "mi zapato ",
+    "el dedo del pie ",
+    "la mano izquierda ",
+    "el ordenador "
+  ];
+  let cuando = [
+    "ayer por la noche",
+    "cuando venia de camino",
+    "mientras conducía",
+    "cuando dormía",
+    "en la tarde que estudiaba"
+  ];
+
+  let excusa1 = Math.floor(Math.random() * culpable.length);
+  let excusa2 = Math.floor(Math.random() * hizo.length);
+  let excusa3 = Math.floor(Math.random() * evitar.length);
+  let excusa4 = Math.floor(Math.random() * cuando.length);
+
+  document.querySelector("#Excusa_aleatoria").innerHTML =
+    excusa +
+    culpable[excusa1] +
+    hizo[excusa2] +
+    evitar[excusa3] +
+    cuando[excusa4];
 };
